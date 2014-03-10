7ec7b25968e94beca6345dae9498c31122e55d8a
Zach Goldberg <zach@zachgoldberg.com>
2014-03-11 00:15:44 +0800

    Basic reconnect functionality works.

--- nm-vpn-reliability/client.py
+++ nm-vpn-reliability/client.py
@@ -1,6 +1,8 @@
 import argparse
 import itertools
+import subprocess
 import sys
+import time
 
 from nmcli import nm, con
 
@@ -62,8 +64,59 @@ def get_servers(args):
 
 
 def server_connect(server):
-    result = con.up(id=server)
-    import pdb; pdb.set_trace()
+    print "Connecting to %s..." % server
+    try:
+        con.up(id=server)
+        print "Connected"
+        return True
+    except:
+        return False
+
+
+def is_nm_active(server):
+    try:
+        results = con.status(id=server)
+    except:
+        return False
+
+    if not results:
+        return False
+
+    results = results[0]
+    return (results.get('STATE') == "activated" and
+            results.get('VPN-STATE') == '5 - VPN connected')
+
+
+def shell(args):
+    """Execute args and returns status code, stdout and stderr
+
+    Any exceptions in running subprocess are allowed to raise to caller
+    """
+    process = subprocess.Popen(args, stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    retcode = process.returncode
+
+    return retcode, stdout, stderr
+
+
+def loop_while_connected(vpnserver, pingserver):
+    had_internet = False
+    while True:
+        if not is_nm_active(vpnserver):
+            print 'VPN dropped on its own!'
+            return had_internet
+
+        print "Start ping to %s" % pingserver
+        retcode, _, _ = shell(["ping", '-c', '2', pingserver])
+        print "Ping Ret: %s" % retcode
+        if retcode:
+            return had_internet
+
+        had_internet = True
+        time.sleep(3)
+
+    return had_internet
 
 
 def vpn_connect(servers,
@@ -73,7 +126,18 @@ def vpn_connect(servers,
     # Step 2: Once connected, verify it works
     # Step 3: If asked to reconnect on failure, do so!
     for server in itertools.cycle(servers):
-        connected = server_connect(server)
+        if not is_nm_active(server):
+            if not  server_connect(server):
+                print "Failed server connect.  Trying another..."
+                continue
+
+        connected = is_nm_active(server)
+        if connected:
+            had_internet = loop_while_connected(server, external_server)
+
+        print had_internet, auto_reconnect
+        if had_internet and not auto_reconnect:
+            return
 
 
 if __name__ == '__main__':
