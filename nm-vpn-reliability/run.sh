24c676ae72de0f60f8885b1a06e0b6f3296121ec
Zach Goldberg <zach@zachgoldberg.com>
2014-04-23 14:46:45 +0800

    One more typo

--- nm-vpn-reliability/run.sh
+++ nm-vpn-reliability/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python client.py -c -v "Ashburn (LOC1 S1)" "Ashville (LOC1 S6)" "Ashville (LOC1 S7)" "Los Angeles (LOC1 S21)"
+python client.py -c -v "Ashburn (LOC1 S1)" "Asheville (LOC1 S6)" "Asheville (LOC1 S7)" "Los Angeles (LOC1 S21)"
