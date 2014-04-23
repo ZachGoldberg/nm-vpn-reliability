70874d0d6e2aeb9e838fcd69141c4085df7e9aa1
Zach Goldberg <zach@zachgoldberg.com>
2014-04-23 14:44:22 +0800

    Minor updates

---
+++ nm-vpn-reliability/run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python client.py -c -v "Ashburn (LOC1 S1)" "Ashburn (LOC1 S6)" "Ashburn (LOC1 S7)" "Los Angeles (LOC1 S21)"
---
+++ nm-vpn-reliability/stop.py
@@ -0,0 +1,11 @@
+import argparse
+import itertools
+import subprocess
+import sys
+import time
+
+from client import kill_vpns
+
+if __name__ == '__main__':
+    kill_vpns()
+
